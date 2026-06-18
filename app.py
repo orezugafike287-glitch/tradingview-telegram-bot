2f843f2d9df544c9fb0c620815ab2b9066601183
orezugafike287-glitch <orezugafike287@gmail.com>
2026-06-18 17:28:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,17 +9,28 @@ CHAT_ID = os.getenv("5023516508")
 
 @app.route("/", methods=["POST"])
 def webhook():
-    data = request.json
+    try:
+        data = request.json
 
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={
-            "chat_id": CHAT_ID,
-            "text": str(data)
-        }
-    )
+        print("DATA:", data)
+        print("BOT_TOKEN:", BOT_TOKEN[:10] if BOT_TOKEN else "NONE")
+        print("CHAT_ID:", CHAT_ID)
 
-    return "ok"
+        r = requests.post(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            json={
+                "chat_id": CHAT_ID,
+                "text": str(data)
+            }
+        )
+
+        print("TELEGRAM:", r.text)
+
+        return "ok"
+
+    except Exception as e:
+        print("ERROR:", str(e))
+        return str(e), 500
 
 @app.route("/", methods=["GET"])
 def home():
